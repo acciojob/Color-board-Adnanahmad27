b12d8a5ea448b01de4d762da7367c3b50d91afc9
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-12 02:30:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+const container = document.querySelector(".container");
+for(let i=0 ; i<800 ; i++){
+	let grid = document.createElement('div');
+	grid.className = "square";
+	grid.addEventListener("mouseover", onMouse);
+//	grid.addEventListener("mouseout", offMouse);
+	//grid.style.backgroundColor = '#333333';
+	container.appendChild(grid);
+}
+function offMouse(e) {
+	
+}
+function onMouse(e) {
+	const color = '0123456789abcdef';
+	let generateColor = '#';
+	for(let i=0 ; i<6 ; i++){
+		generateColor += color[Math.floor(Math.random()*16)];
+	}
+	e.target.style.backgroundColor = generateColor;
+	setTimeout(() => {
+		e.target.style.backgroundColor = '#333333';
+	},1000);
+}
